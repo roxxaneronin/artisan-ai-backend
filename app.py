531773a28857530d345b90f1750ce33f71c65a26
Rakeshvemula <127495750+roxxaneronin@users.noreply.github.com>
2025-09-21 14:26:36 +0530

    Update app.py
    CORS_changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # --- API Keys & Configuration ---
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
